21080e26e7c9aa12e1e04a4d3b06940573ded89d
Banshraj1 <prajapatibanshraj57@gmail.com>
2026-08-13 02:12:49 +0530

    a few origins allowed in cors

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const port = process.env.port;
 const app = express();
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://techedu-eta.vercel.app",
+            "https://techedu-frontend.vercel.app/",
+        ],
+        credentials: true,
+    }),
 );
+// app.use(cors());
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.raw({ limit: "16kb" }));
 app.use(express.static("public"));

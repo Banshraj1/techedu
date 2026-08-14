0c9af9de23addf2f35d86d6980a67fc53383fa5f
Banshraj1 <prajapatibanshraj57@gmail.com>
2026-08-14 15:25:21 +0530

    node modules changed 01

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Working");
 });
+app.get("/test", (req, res) => {
+    res.send("Working test is good");
+});
 
 app.listen(8000, () => {
   console.log("running");
